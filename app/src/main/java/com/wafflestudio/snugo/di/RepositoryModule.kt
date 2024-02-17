32b00caf5b105c0e6b77fd00640ea7614b00da5f
eastshine2741 <eastshine@snu.ac.kr>
2024-02-18 00:28:54 +0900

    lint

--- app/src/main/java/com/wafflestudio/snugo/di/RepositoryModule.kt
+++ app/src/main/java/com/wafflestudio/snugo/di/RepositoryModule.kt
@@ -1,11 +1,11 @@
 package com.wafflestudio.snugo.di
 
+import com.wafflestudio.snugo.features.home.LocationRepository
+import com.wafflestudio.snugo.features.home.LocationRepositoryImpl
 import com.wafflestudio.snugo.repository.BuildingsRepository
 import com.wafflestudio.snugo.repository.BuildingsRepositoryImpl
 import com.wafflestudio.snugo.repository.UserRepository
 import com.wafflestudio.snugo.repository.UserRepositoryImpl
-import com.wafflestudio.snugo.features.home.LocationRepository
-import com.wafflestudio.snugo.features.home.LocationRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -19,7 +19,7 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindBuildingsRepository(buildingsRepositoryImpl: BuildingsRepositoryImpl): BuildingsRepository
-  
+
     @Binds
     abstract fun bindLocationRepository(locationRepositoryImpl: LocationRepositoryImpl): LocationRepository
 }
