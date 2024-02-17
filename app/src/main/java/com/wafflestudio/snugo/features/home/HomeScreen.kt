d4ef8880728055aac1d825bf6ba22c368662958f
양주현 <didwngus01@naver.com>
2024-02-17 21:33:01 +0900

    지도 터치해서 위경도 로그 찍기

--- app/src/main/java/com/wafflestudio/snugo/features/home/HomeScreen.kt
+++ app/src/main/java/com/wafflestudio/snugo/features/home/HomeScreen.kt
@@ -1,10 +1,15 @@
 package com.wafflestudio.snugo.features.home
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,6 +41,9 @@ fun HomeScreen(
     startMoving: () -> Unit,
 ) {
     val cameraPositionState = rememberCameraPositionState()
+    var list by remember {
+        mutableStateOf(listOf<LatLng>())
+    }
 
     Column(
         modifier =
@@ -47,6 +55,9 @@ fun HomeScreen(
             modifier = Modifier.weight(0.5f),
             cameraPositionState = cameraPositionState,
             locationSource = rememberFusedLocationSource(),
+            onMapClick = { _, latLng ->
+                 list = (list + latLng).toMutableList()
+            },
             properties = MapProperties(locationTrackingMode = LocationTrackingMode.Follow),
             uiSettings = MapUiSettings(isLocationButtonEnabled = true),
         ) {
@@ -59,22 +70,59 @@ fun HomeScreen(
                     outlineColor = Color.Green,
                 )
             }
+            if (list.size >=2 ) {
+                PathOverlay(
+                    coords = list,
+                    width = 3.dp,
+                    color = Color.Red,
+                )
+            }
         }
         Text(
             text = "기록 시작",
             modifier =
-                Modifier
-                    .padding(15.dp)
-                    .clickable {
-                        if (pageMode == HomePageMode.NORMAL) {
-                            startMoving()
-                        }
-                    },
+            Modifier
+                .padding(15.dp)
+                .clickable {
+                    if (pageMode == HomePageMode.NORMAL) {
+                        startMoving()
+                    }
+                },
             style =
                 TextStyle(
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
                 ),
+        )
+        Text(
+            text = "로그",
+            modifier =
+            Modifier
+                .padding(15.dp)
+                .clickable {
+                    Log.d("aaaa", list.fold("") {acc, latLng ->
+                        acc + ", LatLng(${latLng.latitude}, ${latLng.longitude})"
+                    })
+                },
+            style =
+            TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+            ),
+        )
+        Text(
+            text = "지우기",
+            modifier =
+            Modifier
+                .padding(15.dp)
+                .clickable {
+                    list = emptyList()
+                },
+            style =
+            TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+            ),
         )
     }
 }
